6e48c8c8aac54ccf8d4daa03e1d07f369c264063
Olexii Lysenko <olysenko@proffiz.com>
2018-11-07 14:28:39 +0300

    cconfig changes

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+// app.use(express.static(".dist/users-angular-app"));
+// app.get("/*", (req, res) => {
+//   // res.sendFile(path.join(_dirname, "/dist/users-angular-app/index.html"));
+//   res.sendFile("index.html", { root: "." });
+// });
+
+// app.listen(process.env.PORT || 8080, () => {
+//   console.log("server starterd from server.js!!");
+// });
+
+//Install express server
 const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.static(".dist/users-angular-app"));
-app.get("/*", (req, res) => {
-  // res.sendFile(path.join(_dirname, "/dist/users-angular-app/index.html"));
-  res.sendFile("index.html", { root: "." });
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/users-angular-app"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/users-angular-app/index.html"));
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log("server starterd from server.js!!");
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
